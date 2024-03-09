0b044822f6c92e6953dc1df51db10815947e677f
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-09 05:38:19 +0100

    if statment now working

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,10 +1,35 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+
+const thescroll = ref(false)
+console.log(thescroll)
+
+
+function handlescroll(){
+    if(thescroll !== true){
+        
+        console.log(" nice")
+    }
+    else{
+       console.log("not nice")
+    }
+  
+
+    // console.log("hello")
+}
+
+onMounted(() => {
+    handlescroll()
+})
+
+
+
     
     
 </script>
 <template>
     
-    <div class="z-50 fixed w-full flex  justify-center items-center border border-black">
+    <div class="z-50 fixed w-full flex  justify-center items-center border border-black " ref="thescroll">
         <div class=" w-5/6  flex py-2">
             <div class=" w-2/6 flex justify-center items-center ">
                 <img src="@/assets/nav-logo.png" alt="" class="w-8 lg:w-10">
--- src/components/theFirst.vue
+++ src/components/theFirst.vue
@@ -3,10 +3,10 @@
 </script>
 <template>
   <div class=" headerimg w-full h-screen relative border-2 border-green-500 ">
-    <div class="border-2 border-green-900  w-3/6 font-textstyle text-9xl tracking-wide font-semibold absolute top-36 left-80 ">
-        <p>LOSANGELES</p>
-        <p>MOUNTAINS</p>
-    </div>
+    <!-- <div class="border-2 border-green-900  w-3/6 font-textstyle text-9xl tracking-wide font-semibold absolute top-36 left-80 "> -->
+        <!-- <p>LOSANGELES</p> -->
+        <!-- <p>MOUNTAINS</p> -->
+    <!-- </div> -->
   </div>
 </template>
 
