000f78b623dcbf2a1d3041f458fd279ea6bbdc5e
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-09 15:38:31 +0100

    tryig to make the accordion

--- src/components/theThird.vue
+++ src/components/theThird.vue
@@ -3,7 +3,47 @@
     
     </script>
 <template>
-    <div  class="w-full h-screen climb">
+    <div  class="climb w-full h-screen  background-container">
+        <div class=" bg-white ">
+            <div class=" w-5/6 lg:w-4/6   flex mx-auto py-3">
+                <div class="  w-2/6 lg:w-2/6 flex justify-center items-center ">
+                    <h1 class="text-4xl lg:text-6xl font-oswald font-bold   ">02 <span>.</span></h1>
+                    <p class="lg:text-2xl font-oswald font-bold  -ml-2 lg:-ml-4 text-darkbluebg">CLIMB</p>
+                </div>
+          
+                <div class="w-4/6  flex justify-start items-center">
+                    <p class="  font-smallertext text-xs  lg:text-base leading-tight ">Cray scelerisque id quam sed Pellentesque urna nunc gravida quis hendrerit ac,<span class="hidden lg:inline" >tistesque ut quam. Vivamuus suscript disnissim totor nec congue.</span> </p>
+                </div>
+
+            </div>
+        </div>
+        <!-- <div class="bg-darkbluebg ">
+            <div class="w-4/6 mx-6 lg:w-3/5 border-green-900 border-2 flex justify-center items-center">
+            
+                <ul class="flex w-full  lg:w-3/6">
+                    <li class="w-1/2  text-sm font-oswald hover:bg-gray-500 hover:text-darkbluebg lg:text-base  text-center"> <span class="border-2 border-yellow-900 border-t-0 border-l-0 border-r-0">MOUNTAIN 1</span></li>
+                    <li class="w-1/2  text-sm font-oswald hover:bg-gray-500 hover:text-darkbluebg lg:text-base  text-center"><span class="border-2 border-yellow-900 border-t-0 border-l-0 border-r-0">MOUNTAIN 2</span></li>
+                </ul>
+            </div>
+        </div> -->
+        <div class="bg-darkbluebg wrap "> 
+            <!-- flex justify-center items-center -->
+            <div class="collapsible w-4/6 mx-6 lg:w-3/5 border-green-900 border-2   ">
+                <input type="checkbox" name="" id="collapsible-head">
+                <label for="collapsible-head" class="text-green-700"> hello</label>
+                <div class="collapsibletext">
+                    <h1 >heading</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum veritatis modi ex ratione? In, repellat. Provident tenetur molestias, nobis inventore, dolorem sed itaque placeat nisi beatae corporis quisquam dicta quis?</p>
+
+                </div>
+            
+                <!-- <ul class="flex w-full  lg:w-3/6"> -->
+                    <!-- <li class="w-1/2  text-sm font-oswald hover:bg-gray-500 hover:text-darkbluebg lg:text-base  text-center"> <span class="border-2 border-yellow-900 border-t-0 border-l-0 border-r-0">MOUNTAIN 1</span></li> -->
+                    <!-- <li class="w-1/2  text-sm font-oswald hover:bg-gray-500 hover:text-darkbluebg lg:text-base  text-center"><span class="border-2 border-yellow-900 border-t-0 border-l-0 border-r-0">MOUNTAIN 2</span></li> -->
+                <!-- </ul> -->
+            </div>
+        </div>
+
         hello
     </div>
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "darkbluebg":"#2c3c4d"
+      }
+    },
     fontFamily:{
       smallertext:["lato"],
       oswald:["Oswald"],
