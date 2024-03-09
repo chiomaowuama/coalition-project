f2ed24abcaa76554ea06aa36f2d0717d0d272817
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-09 01:28:23 +0100

    more progress in it

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,9 +4,17 @@
 </script>
 <template>
     
-    <div>
-        <div>
-            <div></div>
+    <div class=" flex  justify-center items-center border border-black">
+        <div class=" w-5/6  flex py-2">
+            <div class=" w-2/6 flex justify-center items-center ">
+                <img src="@/assets/nav-logo.png" alt="" class="w-8 lg:w-10">
+            </div>
+            <div class="w-4/6 flex flex-col justify-items-end items-end">
+                <ul class="  flex my-auto w-5/6 lg:w-3/6 justify-around text-xs lg:text-sm font-smallertext italic font-semibold">
+                    <li class="border border-black border-t-0 border-l-0 border-r-0">01. HISTORY</li>
+                    <li class="border border-black border-t-0 border-l-0 border-r-0">02. TEAM</li>
+                </ul>
+            </div>
         </div>
 
     </div>
