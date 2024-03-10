5343720d8797bba6429a98f6158df4825ccfd4a0
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-10 03:19:44 +0100

    finally finshed nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -29,15 +29,15 @@ onMounted(() => {
 </script>
 <template>
     
-    <div class="z-50 fixed w-full flex  justify-center items-center " ref="thescroll" :class="scrollmovment > 0 ? 'scrolling' : ''">
+    <div class="z-50 fixed w-full flex  justify-center items-center text-white" ref="thescroll" :class="scrollmovment > 0 ? 'scrolling' : ''">
         <div class=" w-5/6  flex py-2">
             <div class=" w-2/6 flex justify-center items-center ">
                 <img src="@/assets/nav-logo.png" alt="" class="w-8 lg:w-10">
             </div>
             <div class="w-4/6 flex flex-col justify-items-end items-end">
-                <ul class="  flex my-auto w-5/6 lg:w-3/6 justify-around text-xs lg:text-sm font-smallertext italic font-semibold">
-                    <li class="border border-black border-t-0 border-l-0 border-r-0">01. HISTORY</li>
-                    <li class="border border-black border-t-0 border-l-0 border-r-0">02. TEAM</li>
+                <ul class="  flex my-auto w-5/6 lg:w-3/6 justify-around text-xs lg:text-sm font-smallertext italic font-medium">
+                    <li class="border border-white border-t-0 border-l-0 border-r-0">01. HISTORY</li>
+                    <li class="border border-white border-t-0 border-l-0 border-r-0">02. TEAM</li>
                 </ul>
             </div>
         </div>
