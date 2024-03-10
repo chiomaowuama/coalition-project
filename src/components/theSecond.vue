af76fed766bb2846ac1757410658b6445ac52bad
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-10 20:09:12 +0100

    ths second text area done

--- src/components/theSecond.vue
+++ src/components/theSecond.vue
@@ -4,7 +4,19 @@
 <template>
   <div class="w-full h-screen history background-container">
     <!-- <div class="background-container"> -->
-    <h1 class="text-behind">Your Text Here</h1>
+    <div>
+      <div class="w-5/6 lg:w-4/6  flex flex-col mx-auto py-3 ">
+        <div class="w-4/6 lg:w-2/6 flex  px-4 lg:px-0 lg:justify-center items-center  ">
+          <h1 class="text-5xl lg:text-8xl font-oswald font-bold text-footerbluebg opacity-60">01<span>.</span></h1>
+          <p class="lg:text-2xl font-oswald font-bold  -ml-2 lg:-ml-5 -mb-2 lg:-mb-4 text-darkbluebg">CLIMB</p>
+        </div>
+        <div class="w-full pl-4 lg:w-9/12 lg:px-2 font-smallertext text-xs lg:text-sm  mx-auto">
+          <p class="leading-tight ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta illum ratione omnis,<span class="w-2/5  overflow-hidden">praesentium nesciunt ex inventore non commodi sed illo repellat veniam aliquam quod harum. Esse modi eius cumque commodi.</span></p>
+        </div>
+      </div>
+      <div></div>
+
+    </div>
     
   <!-- </div> -->
   </div>
--- src/components/theThird.vue
+++ src/components/theThird.vue
@@ -6,7 +6,7 @@
         <div class=" bg-white ">
             <div class=" w-5/6 lg:w-4/6   flex mx-auto py-3">
                 <div class="  w-2/6 lg:w-2/6 flex justify-center items-center ">
-                    <h1 class="text-4xl lg:text-6xl font-oswald font-bold   ">02 <span>.</span></h1>
+                    <h1 class="text-4xl lg:text-6xl font-oswald font-bold text-footerbluebg opacity-60   ">02 <span>.</span></h1>
                     <p class="lg:text-2xl font-oswald font-bold  -ml-2 lg:-ml-4 text-darkbluebg">CLIMB</p>
                 </div>
           
@@ -21,7 +21,7 @@
             <div class="myaccordion list-none bg-darkbluebg  ">
                 <ul class=" w-5/6 lg:w-4/6 mx-auto flex list-none bg-darkbluebg">
                     
-                    <li  class="float-left relative py-2 hover:bg-gray-500 font-oswald tracking-wide w-3/6  lg:w-1/4 "><a href="" class="bg-darkbuebg text-white font-sm lg:font-base font-bold  text-center block"> <span class="hover:border-2 hover:border-darkbluebg hover:border-l-0 hover:border-r-0 hover:border-t-0  ">MOUNTAIN 1</span></a>
+                    <li  class="float-left relative py-2 hover:bg-gray-500 font-oswald tracking-wide w-3/6  lg:w-1/4 "><a href="#" class="bg-darkbuebg text-white font-sm lg:font-base font-bold  text-center block"> <span class="hover:border-2 hover:border-darkbluebg hover:border-l-0 hover:border-r-0 hover:border-t-0  ">MOUNTAIN 1</span></a>
                         <ul id="Textbox1">
                             <!-- <li  class="float-left relative"><a href="" class="bg-darkbuebg text-white font-base font-bold py-15 px-55 text-center block" >mountain a</a></li> -->
                             <h1 class="font-oswald  text-sm lg:text-2xl font-bold px-4 pt-2 text-darkbluebg">SCHEDULE</h1>
@@ -43,7 +43,7 @@
                         </ul>
     
                     </li>
-                    <li  class="float-left relative py-2 hover:bg-gray-500 font-oswald tracking-wide w-3/6  lg:w-1/4 "><a href="" class="bg-darkbuebg text-white font-sm lg:font-base font-bold py-15 px-55 text-center block"><span class="hover:border-2 hover:border-darkbluebg hover:border-l-0 hover:border-r-0 hover:border-t-0  ">MOUNTAIN 2</span></a>
+                    <li  class="float-left relative py-2 hover:bg-gray-500 font-oswald tracking-wide w-3/6  lg:w-1/4 "><a href="#" class="bg-darkbuebg text-white font-sm lg:font-base font-bold py-15 px-55 text-center block"><span class="hover:border-2 hover:border-darkbluebg hover:border-l-0 hover:border-r-0 hover:border-t-0  ">MOUNTAIN 2</span></a>
                         <ul id="Textbox2" class="">
                             <h1 class="font-oswald text-sm lg:text-2xl font-bold px-4 pt-2 text-darkbluebg">SCHEDULE</h1>
                             <div class="flex justify-between p-2 lg:p-4 items-center">
