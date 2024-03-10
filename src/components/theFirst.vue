6317bf1e277006b564928e581064b88f4d89e5e5
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-10 02:47:00 +0100

    more progress

--- src/components/theFirst.vue
+++ src/components/theFirst.vue
@@ -5,13 +5,16 @@
 
 <template>
   <!-- relative -->
-  <div class=" heading   relative w-full h-screen border-2 border-green-500 overflow-hidden ">
-    <img src="@/assets/header-bg.png" alt="" srcset="" class="border-2  border-red-700 object-cover h-screen lg:h-fit lg:object-contain "/>
+  <!-- overflow-hidden -->
+  <div class=" heading   relative w-full h-screen  overflow-hidden ">
+    <img src="@/assets/header-bg.png" alt="" srcset="" class=" object-cover h-screen lg:h-fit relative z-1 lg:object-contain mix-blend-overlay "/>
+    <img src="@/assets/header-overlay.png" alt="" srcset="" class="absolute top-0 left-0 object-left-top mix-blend-overlay object-contain  h-screen lg:h-fit lg:object-contain"/>
 
 
-     <div  class="  absolute  top-36 lg:top-36 w-3/6 font-textstyle  text-6xl lg:text-9xl tracking-wide font-semibold left-24 lg:left-80"  >
-          <p>LOSANGELES</p>
-          <p>MOUNTAINS</p>
+
+     <div  class="  absolute  top-36 lg:top-36 w-3/6 font-textstyle  text-6xl lg:text-9xl tracking-wide font-semibold left-24 lg:left-80 "  >
+          <p class="">LOSANGELES</p>
+          <p class=" mix-blend-color-burn">MOUNTAINS</p>
         </div>
     {/* <!-- <div class="border-2 border-green-900  w-3/6 font-textstyle text-9xl tracking-wide font-semibold absolute top-36 left-80 "> -->
         <!-- <p>LOSANGELES</p> -->
