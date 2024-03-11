20d0df6b156d6fc59197fd7e7ce507d35e50de04
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-11 04:23:51 +0100

    starting carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,34 +10,43 @@ import footerSection from "@/components/footerSection.vue";
 const theFirstRef = ref(null);
 const theSecondRef = ref(null);
 const theThirdRef = ref(null);
-const link = ref(null)
+const link = ref(null);
+
+
+
 
 const useComponent = (component) => {
-  if(component){
-    scrollIntoComponent(theFirstRef, component);
+  if(component === 'theFirst'){
+    scrollIntoComponent(theFirstRef);
   }
-  if(component){
+  if(component ==='theSecond'){
     scrollIntoComponent(theSecondRef, component);
   }
-  if(component){
-    scrollIntoComponent(theThirdRef, component);
+  if(component === 'theThird'){
+    scrollIntoComponent(theThirdRef);
   }else(
     console.log("no")
     )
-  };
-  const scrollIntoComponent = (linking, component) => {
-  console.log('the  body', link)
-  link.value = linking.component
-  // linking.value = component;
-  // if(linking.value != false){
-  //   console.log(linking.value)
-    
-  // }else{
-  //   console.log("no")
+};
+
+const scrollIntoComponent = (linking) => {
+  // console.log('the  body', linking)
+  // if (linking.value) {
+  //   linking.value.scrollIntoView({ behavior: 'smooth' });
+  // } else {
+  //   console.error("Cannot scroll into component - no valid element found.");
   // }
-      // console.log('Scrolling into view:', myComponentRef.value);
-      //   myComponentRef.value.scrollIntoView({ behavior: 'smooth' });
-    };
+  linking.value.scrollIntoView({ behavior: 'smooth' });
+  linking.value = component;
+  if(linking.value != false){
+    console.log(linking.value)
+    
+  }else{
+    console.log("no")
+  }
+      console.log('Scrolling into view:', myComponentRef.value);
+        myComponentRef.value.scrollIntoView({ behavior: 'smooth' });
+};
 </script>
 
 <template>
